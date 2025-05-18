a1462617ba5988ce437fd30bd868fe314806920a
calvink19 <yt.calvin.coolest@gmail.com>
2025-05-18 14:19:08 -0500

    Delete source directory

--- source/signupwrong.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// man idk why i did it like this
-echo "<script type='text/javascript'>
-        alert('Incorrect username/password.');
-        window.location.href = 'new.php';  // Change this to your desired URL
-      </script>";
-?>
